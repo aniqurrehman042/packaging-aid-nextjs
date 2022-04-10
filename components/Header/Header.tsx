da4c3b4a0382049d7ad07e0e2b8ceb27c6051547
Aniq Ur Rehman <aniqurrehman042@gmail.com>
2022-04-11 01:57:54 +0500

    updated header and with nav with responsiveness

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,15 +1,27 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+    const [currentHeaderImgNo, setCurrentHeaderImgNo] = useState(0);
+
+    useEffect(() => {
+        const bannerImgUpdateIntervalId = setInterval(() => setCurrentHeaderImgNo(oldHeaderImgNo => (oldHeaderImgNo + 1) % 3), 2500);
+        return () => clearInterval(bannerImgUpdateIntervalId);
+    }, []);
+
     return (
         <header>
+            <img className={`header-bg-img ${currentHeaderImgNo === 0 ? 'header-bg-img-active' : ''}`} src={`https://firebasestorage.googleapis.com/v0/b/packagingaid.appspot.com/o/banner-1.webp?alt=media`} />
+            <img className={`header-bg-img ${currentHeaderImgNo === 1 ? 'header-bg-img-active' : ''}`} src={`https://firebasestorage.googleapis.com/v0/b/packagingaid.appspot.com/o/banner-2.webp?alt=media`} />
+            <img className={`header-bg-img ${currentHeaderImgNo === 2 ? 'header-bg-img-active' : ''}`} src={`https://firebasestorage.googleapis.com/v0/b/packagingaid.appspot.com/o/banner-3.webp?alt=media`} />
             <div className="header-content">
                 <h1>Stylish Custom Printed Packaging Boxes for your Products</h1>
                 <p>We help turn your dream boxes into reality in a fast and easy way</p>
-            </div>
-            <div className="action-buttons">
-                <Link href="/quote-form"><a className="btn-get-free-quote">Get Free Quote</a></Link>
-                <a href="#categoriesSection">Get Started</a>
+
+                <div className="action-buttons">
+                    <Link href="/quote-form"><a className="btn-get-free-quote">Get Free Quote</a></Link>
+                    <a href="#categoriesSection">Get Started</a>
+                </div>
             </div>
         </header>
     );
